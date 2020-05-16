74167a25b83ca7e8ed46571f134081a86ae8c7ee
Finn-Thorben Sell <dev@finn-thorben.me>
2020-05-16 22:21:03 +0200

    increase catkinClean capabilities

--- vars/catkinClean.groovy
+++ vars/catkinClean.groovy
@@ -1,3 +1,7 @@
 def call() {
     sh "cd /catkin_ws; catkin clean -y"
+    sh "rm -rf /catkin_ws/install"
+    sh "rm -rf /catkin_ws/devel"
+    sh "rm -rf /catkin_ws/build"
+    sh "rm -rf /catkin_ws/logs"
 }
